3a1ad7b1dc4250240d329bbefed90b47a594649c
Giovanny Jimenez <42052095+giojimen3z@users.noreply.github.com>
2021-02-22 18:13:53 -0500

    feature: adding paginator to list view

--- src/main/java/com/bolsadeideas/springboot/app/SpringBootDataJpaApplication.java
+++ src/main/java/com/bolsadeideas/springboot/app/SpringBootDataJpaApplication.java
@@ -1,4 +1,4 @@
-package com.springbootdatajpa;
+package com.bolsadeideas.springboot.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringBootDataJpaApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(SpringBootDataJpaApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootDataJpaApplication.class, args);
+	}
 
 }
